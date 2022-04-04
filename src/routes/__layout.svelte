92e412def35d9aad056e147d8c1060aee6c810fe
Luciano Ratamero <luciano@ratamero.com>
2022-04-03 21:14:16 -0300

    Fix loading overlay for when people have already scrolled

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -20,8 +20,8 @@
   />
 </svelte:head>
 
-{#if Boolean($navigationIsDelayed)}
-  <div class="absolute w-full h-full z-10" in:fade={{ duration: 150 }}>
+{#if $navigationIsDelayed}
+  <div class="fixed w-full h-full z-10" in:fade={{ duration: 150 }}>
     <div class="absolute w-full h-full bg-white dark:bg-cyan-800 opacity-50 z-10"></div>
     <div class="absolute w-full h-full flex justify-center items-center z-20">
       <Circle3 />
