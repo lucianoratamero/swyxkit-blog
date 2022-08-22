580494f840cbc8e5799a9a0fb4c3ecdb1238f200
Luciano Ratamero <luciano@ratamero.com>
2022-08-21 22:14:53 -0300

    Fix service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
-import { version, build } from '$service-worker';
+import { timestamp, build } from '$service-worker';
 
-const name = `cache-${version}`;
+const name = `cache-${timestamp}`;
 
 self.addEventListener('install', (event) => {
 	// @ts-expect-error
@@ -12,7 +12,7 @@ self.addEventListener('activate', (event) => {
 	event.waitUntil(
 		caches.keys().then(async (keys) => {
 			for (const key of keys) {
-				if (!key.includes(String(version))) caches.delete(key);
+				if (!key.includes(String(timestamp))) caches.delete(key);
 			}
 		})
 	);
