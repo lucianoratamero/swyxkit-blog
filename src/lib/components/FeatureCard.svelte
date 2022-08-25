6a4a28844f5f4f85b446f625aba05e0c2afefff5
Luciano Ratamero <luciano@ratamero.com>
2022-08-25 03:07:24 -0300

    Enable preload at home items

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -6,18 +6,21 @@
 </script>
 
 <a
-	class="w-full transform rounded-xl bg-gradient-to-r from-cyan-400 via-sky-500 to-pink-500 p-1 transition-all hover:scale-[1.01] md:w-1/3 hover:no-underline"
+	class='w-full transform rounded-xl bg-gradient-to-r from-cyan-400 via-sky-500 to-pink-500 p-1 transition-all hover:scale-[1.01] md:w-1/3 hover:no-underline'
 	{href}
-	><div class="flex h-full flex-col justify-between rounded-lg bg-white p-4 dark:bg-gray-900">
-		<div class="flex flex-col justify-between md:flex-row">
+	sveltekit:prefetch
+>
+	<div class='flex h-full flex-col justify-between rounded-lg bg-white p-4 dark:bg-gray-900'>
+		<div class='flex flex-col justify-between md:flex-row'>
 			<h4
-				class="mb-6 w-full text-lg font-medium tracking-tight text-gray-900 dark:text-gray-100 sm:mb-10 md:text-lg"
+				class='mb-6 w-full text-lg font-medium tracking-tight text-gray-900 dark:text-gray-100 sm:mb-10 md:text-lg'
 			>
 				{title}
 			</h4>
 		</div>
-		<div class="capsize flex items-center text-gray-800 dark:text-gray-200">
+		<div class='capsize flex items-center text-gray-800 dark:text-gray-200'>
 			{stringData}
 		</div>
-	</div></a
+	</div>
+</a
 >
