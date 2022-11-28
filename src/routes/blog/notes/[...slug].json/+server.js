4445619a8b8e4e6434bfc46fc1819c5a61a4e53d
Luciano Ratamero <luciano@ratamero.com>
2022-11-27 21:34:00 -0300

    Fix issues with routing and comments

---
+++ src/routes/blog/notes/[...slug].json/+server.js
@@ -0,0 +1,12 @@
+import { json } from '@sveltejs/kit';
+import { process } from "$lib/markdown.js";
+
+export function GET({ params }) {
+  const { slug } = params;
+
+  return json(process(`src/posts/notes/${slug}.md`), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
