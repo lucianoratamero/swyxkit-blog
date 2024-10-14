efc5ebef5e4dae969b4cb64c4c8786d30b21d684
Luciano Ratamero <luciano@ratamero.com>
2024-10-14 01:18:46 -0300

    Add

---
+++ src/routes/oficinas/frontend-basico-para-backenders/+page.svelte
@@ -0,0 +1,7 @@
+<iframe
+	src="https://codesandbox.io/embed/p3mvq2?view=editor+%2B+preview&module=%2Findex.html"
+	style="width:100%; height:100vh; border:0; border-radius: 4px; overflow:hidden;"
+	title="frontend-basico-para-backenders"
+	allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+	sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
